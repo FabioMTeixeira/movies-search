d968842807dda7101a290c5ed7eb3e22157e5346
Fabio <fabiomt.98@hotmail.com>
2023-07-04 14:55:13 -0300

    feat: start project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className='container'>
+    <div>
+      <input type="text" placeholder='Buscar'/>
+      <button>Buscar</button>
+      <button>Limpar</button>
+    </div>
+    </div>
+  )
+}
+
